48ef3111149f10e11d69592f9a40e6926309265e
Tareq Boulakjar <tareq.boulakjar@gmail.com>
2020-12-02 19:14:18 +0100

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hello',
+    loadChildren: () => import('./modules/hello/hello.module').then(m => m.HelloModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/hello/hello-routing.module.ts
+++ src/app/modules/hello/hello-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HelloComponent } from './hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HelloComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
